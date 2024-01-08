009f7fc203f37e620abac8cab325b206a891e0e8
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-01-08 20:12:05 +0600

    firebase confiuration done

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtbzAlFQ_ooe3tDeJhaHVgCGosNoTdMaw",
+  authDomain: "coffee-store-24568.firebaseapp.com",
+  projectId: "coffee-store-24568",
+  storageBucket: "coffee-store-24568.appspot.com",
+  messagingSenderId: "31788145650",
+  appId: "1:31788145650:web:524b5c74e96b5b2b4ea96b",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
