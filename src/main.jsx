b99709162d79b0462e6bed552e3a429d323504f0
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-01-08 20:16:18 +0600

    auth provider and routes updated in mainjsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AddCofffee from "./Components/AddCofffee.jsx";
+import "./index.css";
+import AddCoffee from "./Components/AddCoffee.jsx";
+import ErrorPage from "./Components/ErrorPage.jsx";
+import Signup from "./Components/Signup.jsx";
+import Signin from "./Components/Signin.jsx";
+import AuthProvider from "./Providers/AuthProvider.jsx";
+import Users from "./Components/Users.jsx";
+import UpdateUser from "./Components/UpdateUser.jsx";
 import UpdateCoffee from "./Components/UpdateCoffee.jsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element: <App />,
+    loader: () =>
+      fetch("https://coffee-store-server-1xx3ez9au-anfal11.vercel.app/coffee"),
+    errorElement: <ErrorPage />,
   },
   {
-    path: "/addcoffee",
-    element: <AddCofffee></AddCofffee>,
+    path: "/addCoffee",
+    element: <AddCoffee />,
   },
   {
-    path: "/updateCoffee",
-    element: <UpdateCoffee></UpdateCoffee>,
+    path: "/updateCoffee/:id",
+    element: <UpdateCoffee />,
+    loader: ({ params }) =>
+      fetch(
+        `https://coffee-store-server-1xx3ez9au-anfal11.vercel.app/coffee/${params.id}`
+      ),
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    path: "/users",
+    element: <Users />,
+    loader: () =>
+      fetch("https://coffee-store-server-1xx3ez9au-anfal11.vercel.app/users"),
+  },
+  {
+    path: "/updateUser/:id",
+    element: <UpdateUser />,
+    loader: ({ params }) =>
+      fetch(
+        `https://coffee-store-server-1xx3ez9au-anfal11.vercel.app/users/${params.id}`
+      ),
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
